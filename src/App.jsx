dc4c2487d1d952a83366ffc92a5ee6cdc38a35f8
myntp <119732035+MYNTCODE@users.noreply.github.com>
2023-11-10 21:12:29 +0700

    fix : delete graph test data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import Population from "./Population";
-import LineChart from "./components/LineChart";
 
 function App() {
   return (
     <div>
       <Population />
-      <LineChart />
     </div>
   );
 }
